1ff96a6ecf155a0915bce1e262406aa6995b3b57
DHKim0428 <31202714+DHKim0428@users.noreply.github.com>
2021-10-31 13:38:37 +0900

    minmaxPooler

--- hw3/bert_poolers.py
+++ hw3/bert_poolers.py
@@ -10,10 +10,33 @@ from transformers.models.bert.modeling_bert import (
 class MeanMaxTokensBertPooler(nn.Module):
     def __init__(self, config):
         super().__init__()
-        raise NotImplementedError
+        self.wmmt = nn.Linear(2*config.hidden_size, config.hidden_size) # W_MMT
+        self.activation = nn.Tanh()
+
+        self.hidden_size = config.hidden_size
+        # raise NotImplementedError
 
     def forward(self, hidden_states, *args, **kwargs):
-        raise NotImplementedError
+        norm = hidden_states.norm(dim=2) # [N, T]
+        max_id = torch.argmax(norm, dim=1) # [N]
+        max_id = torch.reshape(max_id, (-1, 1, 1)) # [N, 1, 1]
+        indices = torch.broadcast_to(max_id, (-1, 1, self.hidden_size)) # [N, 1, H]
+        maxT = torch.gather(hidden_states, 1, indices) # [N, 1, H]
+
+        # option 1: sum(T_i) || max(T_i)
+        sumT = torch.sum(hidden_states, dim=1) # [N, H]
+        c_mmt = torch.cat((sumT, maxT.squeeze()), dim=1) # [N, 2H]
+
+        # # option 2: sum((T_i || max(T_i)))
+        # maxT = torch.broadcast_to(maxT, hidden_states.shape) # [N, T, H]
+        # concatT = torch.cat((hidden_states, maxT), dim=2) # [N, T, 2H]
+        # c_mmt = torch.sum(concatT, dim=1) # [N, 2H]
+        
+        pooled_output = self.wmmt(c_mmt) # [N, H]
+        pooled_output = self.activation(pooled_output) # [N, H]
+
+        return pooled_output
+        # raise NotImplementedError
 
 
 class MyBertPooler(nn.Module):
